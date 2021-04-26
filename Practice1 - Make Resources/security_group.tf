e4c0bacc48d666b7fe7d87c1385a23eed5848e57
ChahYang Kim <praise.kim@lgcns365.com>
2021-04-26 16:26:34 +0900

    resource making sample

---
+++ Practice1 - Make Resources/security_group.tf
@@ -0,0 +1,24 @@
+resource "azurerm_network_security_group" "sg_chan_sample" {
+  name                = "sg-chan-sample"
+  location            = azurerm_resource_group.rg_chan_sample.location
+  resource_group_name = azurerm_resource_group.rg_chan_sample.name
+}
+
+resource "azurerm_network_security_rule" "allow_ssh" {
+  name                        = "allow_ssh"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = "{your ip address}"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg_chan_sample.name
+  network_security_group_name = azurerm_network_security_group.sg_chan_sample.name
+}
+
+resource "azurerm_subnet_network_security_group_association" "sgasso_chan_sample" {
+  subnet_id                 = azurerm_subnet.external.id
+  network_security_group_id = azurerm_network_security_group.sg_chan_sample.id
+}
---
+++ Practice1 - Make Resources/vnet.tf
@@ -0,0 +1,21 @@
+resource "azurerm_virtual_network" "vnet_chan_sample" {
+  name                = "vnet-chan-sample"
+  address_space       = ["172.16.0.0/16"]
+  location            = azurerm_resource_group.rg_chan_sample.location
+  resource_group_name = azurerm_resource_group.rg_chan_sample.name
+}
+
+resource "azurerm_subnet" "external" {
+  name                 = "external"
+  resource_group_name  = azurerm_resource_group.rg_chan_sample.name
+  virtual_network_name = azurerm_virtual_network.vnet_chan_sample.name
+  address_prefixes     = ["172.16.0.0/24"]
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.rg_chan_sample.name
+  virtual_network_name = azurerm_virtual_network.vnet_chan_sample.name
+  address_prefixes     = ["172.16.1.0/24"]
+}
+
