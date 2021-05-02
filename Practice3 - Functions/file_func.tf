98ca42ccb38f066876ea113733137d4193e4a356
ChahYang Kim <praise.kim@lgcns365.com>
2021-05-02 20:40:04 +0900

    create vm with file function

---
+++ Practice3 - Functions/file_func.tf
@@ -0,0 +1,48 @@
+resource "azurerm_public_ip" "pip_chan_sample" {
+  name                = "pip-chan-sample"
+  resource_group_name = azurerm_resource_group.rg_chan_sample.name
+  location            = azurerm_resource_group.rg_chan_sample.location
+  allocation_method   = "Static"
+
+}
+
+resource "azurerm_network_interface" "nic_chan_sample" {
+  name                = "nic-chan-sample"
+  location            = azurerm_resource_group.rg_chan_sample.location
+  resource_group_name = azurerm_resource_group.rg_chan_sample.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.web_subnet[0].id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip_chan_sample.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm_chan_sample" {
+  name                = "vm-chan-sample"
+  resource_group_name = azurerm_resource_group.rg_chan_sample.name
+  location            = azurerm_resource_group.rg_chan_sample.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.nic_chan_sample.id,
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/sshkey_chan.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+}
