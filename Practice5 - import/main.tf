1ce84441420cc55d493988e9436a7074cdf8e095
kimmothy <kimmothy@naver.com>
2021-09-25 20:13:07 +0900

    import test

---
+++ Practice5 - import/main.tf
@@ -0,0 +1,7 @@
+terraform{
+    required_version = "v1.0.1"
+}
+
+provider "azurerm" {
+    features {}
+}
---
+++ Practice5 - import/rg.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "rg_chan_test" {
+  name     = "rg-chan-test"
+  location = "koreacentral"
+} 
